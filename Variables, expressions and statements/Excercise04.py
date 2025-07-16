9b45489a6618aa14ecfe6a8c6872dbb8d2761e7d
Akhil Sharma <sharmakhil1704@gmail.com>
2025-07-16 13:43:01 +0530

    Chapter 02 Complete

---
+++ Variables, expressions and statements/Excercise04.py
@@ -0,0 +1,17 @@
+width = int(input("Enter the width: "))
+height = int(input("Enter the height: "))
+
+
+width_floor = width//2
+print(width_floor, type(width_floor))
+
+
+width_div = width/2.0
+print(width_div, type(width_div))
+
+
+height_div  = height/3
+print(height_div, type(height_div))
+
+
+print(1 + 2 * 5)
---
+++ Variables, expressions and statements/Excercise05.py
@@ -0,0 +1,9 @@
+# Write a program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit, and print out the converted temperature.
+
+celsius = float(input("How's it feeling in °C?\n"))
+
+
+fahrenheit = (celsius * (9//5) + 32)
+
+
+print("Damn! it's", fahrenheit, "°F.")
