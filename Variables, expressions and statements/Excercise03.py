e6d309d47877eff946e7710a97bbad7fa7b48a60
Akhil Sharma <sharmakhil1704@gmail.com>
2025-07-16 12:52:37 +0530

    Chapter 01 Complete

---
+++ Variables, expressions and statements/Excercise03.py
@@ -0,0 +1,5 @@
+#Write a program to prompt the user for hours and rate per hour to compute gross pay.
+hours = input("Enter no. of Hours: ")
+rate = input("Enter the Rate: ")
+pay = hours * rate
+print("Pay: ", pay)
